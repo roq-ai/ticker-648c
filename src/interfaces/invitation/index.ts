37988f833c379547449ce5656abca37df1014e0f
roq-ai <>
2023-06-22 13:10:44 +0000

    Initial commit

---
+++ src/interfaces/invitation/index.ts
@@ -0,0 +1,23 @@
+import { OrganizationInterface } from 'interfaces/organization';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface InvitationInterface {
+  id?: string;
+  email: string;
+  organization_id?: string;
+  user_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  organization?: OrganizationInterface;
+  user?: UserInterface;
+  _count?: {};
+}
+
+export interface InvitationGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  email?: string;
+  organization_id?: string;
+  user_id?: string;
+}
